d604322c968a7a17bbba389e2eb9465a8217a264
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2025-04-04 10:11:07 +0200

    Adjust toc header slightly

--- teachbooks/external_content/process_toc.py
+++ teachbooks/external_content/process_toc.py
@@ -108,7 +108,7 @@ def write_toc_yaml(
     """
     with open(path, encoding=encoding, mode="w") as handle:
         if header is not None:
-            handle.write(f"#{header}\n")
+            handle.write(f"# {header}\n")
         yaml.safe_dump(data, handle)
 
 
