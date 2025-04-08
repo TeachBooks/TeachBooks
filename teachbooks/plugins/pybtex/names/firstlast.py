272a586aea6566a4f25c1e36d4294615c7e7d35f
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2025-04-08 08:15:02 +0200

    Move plugins module to Google style docstrings

--- teachbooks/plugins/pybtex/names/firstlast.py
+++ teachbooks/plugins/pybtex/names/firstlast.py
@@ -8,28 +8,29 @@ from pybtex.style.template import join
 class FirstLastStyle(BaseNameStyle):
 
     def format(self, person, abbr=False):
-        r"""
-        Format names similarly to {vv~}{ll}{, jj}{, f.} in BibTeX.
+        r"""Format names similarly to {vv~}{ll}{, jj}{, f.} in BibTeX.
 
-        >>> from pybtex.database import Person
-        >>> name = Person(string=r"Charles Louis Xavier Joseph de la Vall{\'e}e Poussin")
-        >>> firstlast = NameStyle().format
+        Examples:
+            
+            >>> from pybtex.database import Person
+            >>> name = Person(string=r"Charles Louis Xavier Joseph de la Vall{\'e}e Poussin")
+            >>> firstlast = NameStyle().format
 
-        >>> print(firstlast(name).format().render_as('latex'))
-        Charles Louis Xavier~Joseph de~la Vall{é}e~Poussin
-        >>> print(firstlast(name).format().render_as('html'))
-        Charles Louis Xavier&nbsp;Joseph de&nbsp;la Vall<span class="bibtex-protected">é</span>e&nbsp;Poussin
+            >>> print(firstlast(name).format().render_as('latex'))
+            Charles Louis Xavier~Joseph de~la Vall{é}e~Poussin
+            >>> print(firstlast(name).format().render_as('html'))
+            Charles Louis Xavier&nbsp;Joseph de&nbsp;la Vall<span class="bibtex-protected">é</span>e&nbsp;Poussin
 
-        >>> print(firstlast(name, abbr=True).format().render_as('latex'))
-        C.~L. X.~J. de~la Vall{é}e~Poussin
-        >>> print(firstlast(name, abbr=True).format().render_as('html'))
-        C.&nbsp;L. X.&nbsp;J. de&nbsp;la Vall<span class="bibtex-protected">é</span>e&nbsp;Poussin
+            >>> print(firstlast(name, abbr=True).format().render_as('latex'))
+            C.~L. X.~J. de~la Vall{é}e~Poussin
+            >>> print(firstlast(name, abbr=True).format().render_as('html'))
+            C.&nbsp;L. X.&nbsp;J. de&nbsp;la Vall<span class="bibtex-protected">é</span>e&nbsp;Poussin
 
-        >>> name = Person(first='First', last='Last', middle='Middle')
-        >>> print(firstlast(name).format().render_as('latex'))
-        First~Middle Last
-        >>> print(firstlast(name, abbr=True).format().render_as('latex'))
-        F.~M. Last
+            >>> name = Person(first='First', last='Last', middle='Middle')
+            >>> print(firstlast(name).format().render_as('latex'))
+            First~Middle Last
+            >>> print(firstlast(name, abbr=True).format().render_as('latex'))
+            F.~M. Last
 
         """
         return join[
