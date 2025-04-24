f8bde7fb75a98ad95f6989c4247f958335efd2ab
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2025-04-24 12:03:28 +0200

    Fix tests

--- tests/test_external_content/test_integration.py
+++ tests/test_external_content/test_integration.py
@@ -10,8 +10,8 @@ WORK_DIR = Path(__file__).parent / ".teachbooks"
 PATH_TESTDATA = Path(__file__).parent / "testbook"
 
 ADMONITION_HTML = (
-    "<div class=\"admonition attention\">"
-    "<p class=\"admonition-title\">Attention</p>"
+    "<div class=\"attribution admonition\">"
+    "<p class=\"admonition-title\">Attribution</p>"
     "<p>This page originates from a TeachBook hosted at"
 )
 
