0f1da8cd763e66a18a38025a1f302b06cf652a79
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2025-07-03 16:23:04 +0200

    Ruff lint error fix

--- teachbooks/external_content/process_toc.py
+++ teachbooks/external_content/process_toc.py
@@ -181,7 +181,7 @@ def external_to_local(
     mapping_local["file"] = os.path.join(rel_path, content_file).replace("\\", "/")
 
     for k, v in list(mapping_local.items()):
-        if isinstance(v, (dict, list)):
+        if isinstance(v, dict | list):
             mapping_local[k] = modify_field(
                 v, "external", external_to_local, external_path=external_path, root=root
             )
