e7d4b3a70fb4b4ebe18da6d3c0dd6881e4625c86
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2025-07-18 09:34:52 +0200

    ruff fix

--- teachbooks/external_content/config.py
+++ teachbooks/external_content/config.py
@@ -41,7 +41,11 @@ def get_teachbooks_config(config_file: Path) -> dict[str, str]:
             cfg["attribution_location"] = loc
     
     # Extract language from Sphinx configuration
-    if "sphinx" in config and "config" in config["sphinx"] and "language" in config["sphinx"]["config"]:
+    if (
+        "sphinx" in config
+        and "config" in config["sphinx"]
+        and "language" in config["sphinx"]["config"]
+    ):
         cfg["language"] = config["sphinx"]["config"]["language"]
     
     return cfg
