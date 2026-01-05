8fbaa3c95b0108d5e7bebd51fe40e7a4f90653d3
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2026-01-05 10:31:45 +0100

    Add start comment to inserted Teachbooks headers
    Prepends a start comment marker to Teachbooks headers in both Markdown and reStructuredText files for clearer delimitation of inserted content.

--- teachbooks/external_content/headers.py
+++ teachbooks/external_content/headers.py
@@ -95,8 +95,9 @@ def prepend(file: Path, text: str):
 
 def add_md_admonition(file: Path, header: str):
     """Add an admonition containing `text` to the top of markdown file `file`."""
+    start_of_header_comment = "<!-- Start of inserted Teachbooks header -->"
     end_of_header_comment = "<!-- End of inserted Teachbooks header -->"
-    header += f"\n\n{end_of_header_comment}\n\n"
+    header = f"{start_of_header_comment}\n\n{header}\n\n{end_of_header_comment}\n\n"
     prepend(file, header)
 
 def add_rst_admonition(file: Path, header: str):
@@ -105,8 +106,9 @@ def add_rst_admonition(file: Path, header: str):
     To do this we make use of the `include` directive and write the admonition
     as a separate markdown file which will be parsed by myst.
     """
+    start_of_header_comment = ".. Start of inserted Teachbooks header"
     end_of_header_comment = ".. End of inserted Teachbooks header"
-    header += f"\n\n{end_of_header_comment}\n\n"
+    header = f"{start_of_header_comment}\n\n{header}\n\n{end_of_header_comment}\n\n"
     admon_file = file.parent / f"_ad-{file.stem}.md"
     admon_file.write_text(header, encoding="utf-8")
 
