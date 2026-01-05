ba72bd9184a00bcf4e80928f580f4a38d102642f
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2026-01-05 11:19:55 +0100

    Make integration test print full strack trace to ease debugging

--- tests/test_external_content/test_integration.py
+++ tests/test_external_content/test_integration.py
@@ -1,5 +1,6 @@
 import shutil
 from pathlib import Path
+import traceback
 
 import pytest
 from click.testing import CliRunner
@@ -53,6 +54,9 @@ def test_build(cli: CliRunner, tmp_path: Path, monkeypatch):
         bookdir.as_posix(),
         env={"NO_GIT_CLONE": ""},
     )
+
+    if build_result.exit_code == 1:
+        traceback.print_tb(build_result.exc_info[2])
     assert build_result.exit_code == 0, build_result.output
 
     indexfile = bookdir / "_build" / "html" / "index.html"
